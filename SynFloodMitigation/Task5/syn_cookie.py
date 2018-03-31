815bdbb43a2b08b90e1c283d7a691ffda62c34d0
shivanandkohalli <shivanandk58@gmail.com>
2018-03-31 02:37:16 +0200

    Basic TCP SYN cookie implementation

--- SynFloodMitigation/Task5/syn_cookie.py
+++ SynFloodMitigation/Task5/syn_cookie.py
@@ -14,6 +14,24 @@ PACKET_SYN_ACK = 1
 PACKET_ACK = 2
 PACKET_UNKOWN = -1
 
+STATE_SERVER_CONN = 1 # Client has completed its syn cookie verification, connection with server pending
+STATE_WHITELIST = 2 # Client IP is whitelisted, can communicate with server now
+STATE_NOT_WHITE_LIST = 3 # IP is not yet whitelisted
+
+ip_whitelist_table = {}
+
+# REturn the state of the IP from the whitelist table
+def get_ip_state(ip):
+	global ip_whitelist_table
+	if ip in ip_whitelist_table:
+		return ip_whitelist_table[ip]
+	else:
+		return STATE_NOT_WHITE_LIST
+
+# Add the state of the IP address to the table 
+def add_to_whitelist_ip(ip, state):
+	global ip_whitelist_table
+	ip_whitelist_table[ip] = state
 
 # Returns whether it is a PACKET_SYN, PACKET_SYN_ACK, PACKET_ACK packet
 def return_packet_type(pkt):
@@ -46,11 +64,21 @@ def send_syn_ack_client(pkt):
 	send(resp_syn_ack,iface="ens33")
 
 
+def send_syn_server(pkt):
+	conf.L3socket = L3RawSocket
+	resp_syn = IP(src=pkt['IP'].src, dst=pkt['IP'].dst)/TCP(sport=pkt['TCP'].sport, dport=pkt['TCP'].dport, flags="S", seq=pkt['TCP'].seq-2)
+	send(resp_syn,iface="lo")
+
+
 def verify_client_ack(pkt):
-	print ("ACK received")
 	print (pkt['TCP'].ack)
+	add_to_whitelist_ip(pkt['IP'].src,STATE_SERVER_CONN)
+	send_syn_server(pkt)
 
-
+def send_ack_server(pkt):
+	conf.L3socket = L3RawSocket
+	resp_syn = IP(src=pkt['IP'].dst, dst=pkt['IP'].src)/TCP(sport=pkt['TCP'].dport, dport=pkt['TCP'].sport, flags="A", seq=pkt['TCP'].ack,ack=pkt['TCP'].seq + 1)
+	send(resp_syn,iface="lo")	
 
 # This is the callback function called whenver a new packet is received from a client
 def client_stream(packet):
@@ -58,26 +86,68 @@ def client_stream(packet):
 	pkt = IP(packet.get_payload())
 	retval = return_packet_type(pkt)
 
-	if(retval == PACKET_SYN):
+	if(get_ip_state(pkt['IP'].src)==STATE_WHITELIST):
+		print("whitelisted")
+		packet.accept()
+	elif(retval == PACKET_SYN and (get_ip_state(pkt['IP'].src)==STATE_NOT_WHITE_LIST)): # Client trying for the first time
+		print("Client SYN packet")
 		send_syn_ack_client(pkt)
-	elif(retval == PACKET_ACK):
+	elif(retval == PACKET_SYN and (get_ip_state(pkt['IP'].src)==STATE_SERVER_CONN)): # This is the packet sent by IDS itself, allow it to communicate to server
+		print("Client, LOCAL SYN packet")
+		packet.accept()
+	elif(retval == PACKET_ACK and (get_ip_state(pkt['IP'].src)==STATE_NOT_WHITE_LIST)): # Client trying for the first time
+		print("ACK received from client")
 		verify_client_ack(pkt)
+	elif(retval == PACKET_ACK and (get_ip_state(pkt['IP'].src)==STATE_SERVER_CONN)): # This is the packet sent by IDS itself, allow it to communicate to server
+		print("ACK from LOCKAL IDS")
+		packet.accept()
 	else:
 		print ("Unkown packet")
+		packet.drop()
 	# packet.accept()
+
 
 # This is the callback function called whenver a new packet is being sent from server
 def server_stream(packet):
-	print ("server steam")
+	print ("server stream")
+	pkt = IP(packet.get_payload())
+	retval = return_packet_type(pkt)
+
+	if(get_ip_state(pkt['IP'].dst) ==STATE_WHITELIST):
+		print("Sending to whitelisted ip from server")
+		packet.accept()
+	elif(retval == PACKET_SYN_ACK):
+		print("Recevied SYN ACK from server")
+		send_ack_server(pkt)	
+		add_to_whitelist_ip(pkt['IP'].dst,STATE_WHITELIST)
+	else:
+		print ("Received unkown packet from server")	
+		packet.accept() # Accept the packet sent from the server if not SYN ACK
 	# packet.accept()
 
+# This thread is to start reading all the data from server
+def start_server_com():
+	server = NetfilterQueue()
+	server.bind(1, server_stream) 
+	try:
+		server.run()
+	except KeyboardInterrupt:
+		server.unbind()
+
+# add_to_whitelist_ip('192.168.0.102',STATE_WHITELIST)
+# For cleanly exiting the thread
+active = threading.Event()
+active.set()
+# Create and start the thread for the rate limitter
+thread = threading.Thread(target=start_server_com)
+thread.start()
 
 # Start receving packets from the nfqueue
 client = NetfilterQueue()
-server = NetfilterQueue()
+
 # Packets will be in queue 0, and the callback is client_stream
 client.bind(0, client_stream) 
-server.bind(1, server_stream) 
+
 
 try:
     client.run()
